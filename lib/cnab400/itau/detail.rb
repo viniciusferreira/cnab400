8392b4b9d8ff1218b4a77a23e7501bd3cb7ca754
Felipe Pavão <contact@felipepavao.com>
2015-05-14 15:11:26 -0300

    testing detail string content

---
+++ lib/cnab400/itau/detail.rb
@@ -0,0 +1,3 @@
+class DetailItau
+
+end
--- lib/cnab400/itau/itau.rb
+++ lib/cnab400/itau/itau.rb
@@ -1,12 +1,22 @@
 class Itau
 	attr_accessor :file_path
 	attr_accessor :header
-
+	attr_accessor :details
+	attr_accessor :details_string
 	attr_accessor :trailer
 	
 	def initialize(file_path)
 		@file_path 	= file_path
-		@header 	= ItauHeader.new(File.open(file_path).first)
-		@trailer 	= ItauTrailer.new(File.open(file_path).to_a.last)
+		
+		@header 		= ItauHeader.new(File.open(file_path).first)
+		@trailer 		= ItauTrailer.new(File.open(file_path).to_a.last)
+
+		@details		= []
+
+		details_string_tmp = File.open(file_path).to_a
+		details_string_tmp.shift
+		details_string_tmp.pop
+		
+		@details_string	= details_string_tmp.join("")
 	end
 end
